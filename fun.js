ab76f71f46ef2676a8e1f33432ac937fe2393014
N SATISH <162682521+SATII2004@users.noreply.github.com>
2024-12-25 17:59:28 +0530

    hi

---
+++ fun.js
@@ -0,0 +1,24 @@
+function avg(a,b)
+{
+    return (a+b)/2;
+}
+ 
+
+var vc = avg(13,4);
+
+console.log(vc);
+
+
+
+ function sam(a,s,w){
+
+    d= [(a+s-w)*w ]+ 15 +6;
+
+    return d;
+    
+
+ }
+
+ k= sam(12,22,7);
+
+ console.log(k);
