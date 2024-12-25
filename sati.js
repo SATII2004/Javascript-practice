f6b07f81e5264b03475aa686dbcac776c5c58cc0
N SATISH <162682521+SATII2004@users.noreply.github.com>
2024-12-25 16:31:24 +0530

    Update sati.js

--- sati.js
+++ sati.js
@@ -1,1 +1,47 @@
-console.log("Hii I am Satish.");
+console.log("Hello from the script!");
+console.warn("This is a warning.");
+console.clear()
+console.assert(4==2)
+
+
+//number
+
+var num1=12;
+var num2= 43;
+console.log(num1+num2);
+
+
+//strings
+
+var str1= "Chanakya is a great scholar.";
+var str2= 'Dronacharya is a great guru.';
+
+console.log(str1);
+console.log(str2);
+
+//objects
+var marks={
+    ravi:34,
+    subham:23,
+    fraghav:90
+}
+
+console.log(marks);
+
+
+
+//boolean
+
+var a= true;
+
+console.log(a);
+
+//array
+
+var arr=[2,3,7,9,43,6];
+
+console.log(arr[2]);
+
+for(var i=0;i<6;i++){
+    console.log(arr[i]);
+}
