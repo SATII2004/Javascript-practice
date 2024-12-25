c6e90f74c3cb76dbf8415301a34887839fab606f
N SATISH <162682521+SATII2004@users.noreply.github.com>
2024-12-25 17:35:43 +0530

    HII

---
+++ hello.js
@@ -0,0 +1,32 @@
+//Arithmetic operators (same for -,*,/)
+var a=60;
+var b=30 ;
+console.log("The value od a+b is: "+ (a+b));
+
+//Assignment Operators  (same for -,*,/)
+
+var c=13;
+
+c+=2;
+console.log(c);
+
+
+// Comparison operators (same for  >= ,<= ,==,<,>)
+console.log(a==b);  // checks and returns true or false
+
+
+//Logical operators (&&, ||)
+
+console.log(true && true);
+console.log(true && false);
+
+console.log(!false);
+
+
+
+
+
+
+
+
+
