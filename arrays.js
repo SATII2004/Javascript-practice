76bdecb691ba76cf273176323a03d491b7343fb5
N SATISH <162682521+SATII2004@users.noreply.github.com>
2024-12-26 18:04:44 +0530

    Array and String methods

---
+++ arrays.js
@@ -0,0 +1,38 @@
+let arr=["Fan","Camera",34,null,true];
+
+ // Array Methods
+
+ console.log(arr.length);
+
+ arr.shift();
+
+ 
+
+ console.log(arr);
+
+
+
+ const newLen = arr.unshift("HII");
+
+console.log(newLen);
+ console.log(arr);
+
+ d=[1,4,6,43,23,34224];
+
+ console.log(d);
+
+ d.sort();
+
+ console.log(d);
+
+
+
+ // String methods
+
+ let str = "Hii bro how are you?";
+
+console.log(str.length);
+console.log(str.indexOf("bro"));
+console.log(str.slice(1,3));
+
+console.log(str.replace("Hii","Hello"));
