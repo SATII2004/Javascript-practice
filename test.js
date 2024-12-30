228b1c05b7d5524e1d675f1385cb5aec84a42609
N SATISH <162682521+SATII2004@users.noreply.github.com>
2024-12-30 20:49:19 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
 console.log("Hii js is working...");
 document.writeln("Satish");
+
+let j=0;
+const m =19;
+
+console.log("-------------------------------------");
+
+
+while(j<arr.length)
+{
+    console.log(arr[j]);
+    j++;
+    
+}
