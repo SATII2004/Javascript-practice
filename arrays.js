4403d10052c72e72e02e3ba7586d295b13c77e0f
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-01-08 20:02:44 +0530

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+/*
 let arr=["Fan","Camera",34,null,true];
 
  // Array Methods
@@ -36,3 +37,43 @@ console.log(str.indexOf("bro"));
 console.log(str.slice(1,3));
 
 console.log(str.replace("Hii","Halo"));
+
+*/
+let num=[1,2,3,4,7]
+
+let b= num.toString()
+
+console.log(b,typeof b)
+
+let c= num.join("_")
+
+console.log(c, typeof c)
+
+
+// Map method
+
+let marr = [45,23,29]
+
+marr.map((value,index,array) =>{
+    console.log(value,index,array)
+})
+
+
+
+//Filter method
+
+let marr2 = [23,1,4,90,83,0,76]
+let a2 = marr2.filter((value)=>{
+    return value<10
+})
+
+console.log(a2)
+
+
+//Reduce method
+
+let n = marr2.reduce((i,j)=>{
+    return i+j
+})
+
+console.log(n)
